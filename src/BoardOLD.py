ae8b4dcc62e6e90d54d028585cde27982e522e18
INPECO\giulio.giorgio <giulio.giorgio@inpeco.com>
2019-03-12 17:14:43 +0100

    deletion of BoardOLD.py

--- src/BoardOLD.py
+++
@@ -1,9 +0,0 @@
-class Board():
-    def __init__(self):
-        self.hash = 'hash'
-
-    def available_moves(self):
-        return (0, 1, 2, 3)
-
-    def play(self, player, action):
-        return 1
